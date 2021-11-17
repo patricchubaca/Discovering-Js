40bfcea43dd08ced02bd874c60f8da7aee625e10
patricchubaca <patric_job@outlook.com>
2021-11-17 12:53:35 -0300

    Lendo um DOC com Fetch

--- API-HTTP/index.js
+++ API-HTTP/index.js
@@ -1,5 +1,5 @@
 function puxarPost() {
-  fetch('https://pokeapi.co/api/v2/pokemon/')
+  fetch('https://pokeapi.co/api/v2/pokemon/3')
     .then(r => r.json())
     .then(r => {
       console.log(r)
@@ -7,3 +7,15 @@ function puxarPost() {
 }
 
 puxarPost();
+
+
+function acessarTXT() {
+  fetch('./doc.txt')
+    .then(r => r.text())
+    .then(r => {
+      console.log(r)
+    })
+}
+
+acessarTXT();
+
