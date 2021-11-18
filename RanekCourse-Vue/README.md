7d4cb9d5899291ba0d52340714e4ed8eac1c0b06
patricchubaca <patric_job@outlook.com>
2021-11-18 15:28:53 -0300

    Atualizações

---
+++ RanekCourse-Vue/README.md
@@ -0,0 +1,24 @@
+# raneck
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles Json
+```
+npm run backend
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ RanekCourse-Vue/src/components/ProdutosBuscar.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <form>
+      <input name="busca" id="busca" type="text" v-model="busca" />
+      <input
+        type="submit"
+        id="lupa"
+        value="Buscar"
+        @click.prevent="buscarProdutos(busca)"
+      />
+   
+    </form>
+  </section>
+</template>
+
+<script>
+import { api } from "@/services.js";
+
+export default {
+  data() {
+    return {
+      busca: ""
+    };
+  },
+  methods: {
+   buscarProdutos(){
+     this.$router.push({query:{q:this.busca}})
+   }
+  },
+};
+</script>
+
+<style scoped>
+</style>
