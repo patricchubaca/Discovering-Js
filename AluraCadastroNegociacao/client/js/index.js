cca82924ca229d6d6c9e1f01026244089e7cc502
Patric Enderson <patric_job@outlook.com>
2022-02-23 20:06:26 -0300

    Date capture :: IMC calculation

--- AluraCadastroNegociacao/client/js/index.js
+++ AluraCadastroNegociacao/client/js/index.js
@@ -18,6 +18,7 @@ document.querySelector('.form').addEventListener('submit', function(event){
         td.textContent = campo.value;
         tr.appendChild(td);
     });
+    
     var tdVolume = document.createElement('td');
     tdVolume.textContent = campos[1].value * campos[2].value;
 
--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,5 +1,8 @@
 var paciente = document.querySelector("#primeiro-paciente")
 
+var tdNome = paciente.querySelector(".info-nome")
+var nome = tdNome.textContent;
+
 var tdPeso = paciente.querySelector(".info-peso")
 var peso = tdPeso.textContent;
 
@@ -16,7 +19,37 @@ var imc = peso / (altura * altura);
 
 tdImc.textContent = imc;
 
+
+
+
+console.log(`O peso do ${nome} é ${peso} Kg então seu IMC é igual = ${imc}`);
+console.log(altura);
+console.log(imc);
+console.log(gordura);
+
+var segundoPaciente = document.querySelector("#segundo-paciente");
+
+var tdPeso = segundoPaciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var segundotdAltura = segundoPaciente.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+
+var tdImc = segundoPaciente.querySelector(".info-imc")
+var imc = tdImc.textContent;
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
 console.log(peso);
 console.log(altura);
 console.log(imc);
 console.log(gordura);
+
+
+
+
+
+
+
