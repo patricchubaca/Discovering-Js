c9a40177131843bc3cfbc2f684dea76f59ac0abf
Patric Enderson <patric_job@outlook.com>
2022-02-23 18:14:22 -0300

    Date capture :: IMC calculation

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,14 +1,22 @@
 var paciente = document.querySelector("#primeiro-paciente")
 
-
 var tdPeso = paciente.querySelector(".info-peso")
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura")
 var altura = tdAltura.textContent;
 
+var tdImc = paciente.querySelector(".info-imc")
+var imc = tdImc.textContent;
+
+var tdGordura = paciente.querySelector(".info-gordura")
+var gordura = tdGordura.textContent;
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
 console.log(peso);
 console.log(altura);
-
-
-console.log(paciente)
+console.log(imc);
+console.log(gordura);
