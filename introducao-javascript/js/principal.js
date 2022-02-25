adb5061496b164598a43bffdb0aac7f1809e885d
Patric Enderson <patric_job@outlook.com>
2022-02-25 07:10:03 -0300

    Calculation :: Body mass index

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,7 +1,7 @@
 
 var pacientes = document.querySelectorAll(".paciente")
 
-for(var i = 0; i < pacientes.length; i++){
+for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
 
@@ -11,7 +11,7 @@ for(var i = 0; i < pacientes.length; i++){
     var tdPeso = paciente.querySelector(".info-peso")
     var peso = tdPeso.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura") 
+    var tdAltura = paciente.querySelector(".info-altura")
     var altura = tdAltura.textContent;
 
     var tdImc = paciente.querySelector(".info-imc")
@@ -23,17 +23,17 @@ for(var i = 0; i < pacientes.length; i++){
     var pesoValido = true;
     var alturaValida = true;
 
-if(peso < 0 || peso > 1000){
-    console.log("peso invalido");
-    pesoValido = false;
-    tdImc.textContent = "Peso Invalido";
-}
-if(pesoValido == true){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = Math.round(imc);
-  
-    console.log(`O peso do ${nome} é ${peso} Kg então seu IMC é igual = ${imc}`);
-}
+    if (peso < 0 || peso > 1000) {
+        console.log("peso invalido");
+        pesoValido = false;
+        tdImc.textContent = "Peso Invalido";
+    }
+    if (pesoValido == true) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = Math.round(imc);
+
+        console.log(`O peso do ${nome} é ${peso} Kg então seu IMC é igual = ${imc}`);
+    }
 }
 
 
